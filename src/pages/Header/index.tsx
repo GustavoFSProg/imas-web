8a539abb8d33fea5d56211458b8bcac4c3e1bec6
Gustavo <gustavo.prog40@gmail.com>
2021-03-23 15:47:12 -0300

    headerFeito

---
+++ src/pages/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container, Image, ImageFooter } from './styles'
+
+function Header() {
+  return (
+    <Container>
+      <Image> ÍMÃ</Image>
+      <ImageFooter>gens</ImageFooter>
+    </Container>
+  )
+}
+
+export default Header
---
+++ src/pages/Header/styles.tsx
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  height: 80px;
+  background: #f77d27;
+  /* background: yellow; */
+
+  display: flex;
+  align-items: center;
+  text-align: center;
+  text-align: center;
+  justify-content: center;
+`
+export const Image = styled.p`
+  position: absolute;
+  width: 110px;
+  height: 56px;
+  left: 387px;
+  /* top: 12px; */
+
+  font-family: Chonburi;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 47px;
+  line-height: 0px;
+  /* or 0% */
+
+  display: flex;
+  align-items: center;
+  text-align: center;
+  letter-spacing: 0.01em;
+
+  color: #ffffff;
+`
+
+export const ImageFooter = styled.p`
+  position: absolute;
+  width: 110px;
+  height: 56px;
+  left: 481px;
+  top: -17px;
+
+  font-family: Handlee;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 37px;
+  line-height: 0px;
+  /* or 0% */
+
+  display: flex;
+  align-items: center;
+  text-align: center;
+  letter-spacing: 0.01em;
+
+  color: #ffffff;
+`
